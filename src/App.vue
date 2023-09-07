bc621b5243cfa73c200c93d96e46f866f3823e31
umbertofaenza <umberto.faenza@gmail.com>
2023-09-07 15:16:55 +0200

    first Vite app

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+
+export default {
+  components: { AppTitle },
+};
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+  <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped></style>
